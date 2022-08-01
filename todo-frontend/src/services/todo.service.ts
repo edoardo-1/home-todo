2ab3da5134fc69dc9107ead6912c2ae2a6eccb22
Martin Podhora <empodhora@seznam.cz>
2022-08-01 13:25:46 +0200

    Add todo service

--- todo-frontend/src/services/todo.service.ts
+++ todo-frontend/src/services/todo.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Todo } from 'src/models/todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  private todos$ = new BehaviorSubject({});
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.todos$.next(this.getTodos);
+  }
+
+  //refactor later to get data from api
+  public getTodos: Observable<Todo> = of({
+    id: 2,
+    content: 'todo1',
+    isCompleted: false,
+  });
 }
