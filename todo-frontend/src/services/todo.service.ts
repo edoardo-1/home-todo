27ab2f83d0146f014f340c13774c058e06788bce
Martin Podhora <empodhora@seznam.cz>
2022-08-02 11:30:59 +0200

    Add addTodo mehod

--- todo-frontend/src/services/todo.service.ts
+++ todo-frontend/src/services/todo.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of} from 'rxjs';
 import { Todo } from 'src/models/todo';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private todos$ = new BehaviorSubject({});
+  public todos$ = new BehaviorSubject<Todo[]>([]);
 
   constructor(private httpClient: HttpClient) {
-    this.todos$.next(this.getTodos);
+    this.getTodos.subscribe((data) => this.todos$.next(data));
   }
 
   //refactor later to get data from api
-  public getTodos: Observable<Todo> = of({
-    id: 2,
-    content: 'todo1',
-    isCompleted: false,
-  });
+  public getTodos: Observable<Todo[]> = of([
+    {
+      id: 2,
+      content: 'todo1',
+      isCompleted: false,
+    },
+  ]);
+
+  addNewTodo(newContent : string): void {
+    let newId : number = ~~(Math.random() * 100000)
+    this.todos$.next([...this.todos$.getValue(), {id: newId, content: newContent, isCompleted: false }]);
+  }
 }
