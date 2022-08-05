7c4241bfb2a62e3c3274067d7004e0bb9f62c140
Martin Podhora <empodhora@seznam.cz>
2022-08-05 12:44:19 +0200

    Quick path fix

--- todo-frontend/server.js
+++ todo-frontend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static("./dist/frontend"));
+app.use(express.static("./dist/todo-frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/frontend/" });
+  res.sendFile("index.html", { root: "dist/todo-frontend/" });
 });
 app.listen(process.env.PORT || 8080, () => {
   console.log("app running");
